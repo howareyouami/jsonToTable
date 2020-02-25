a02930ba845ffe74686586614cf2f3ef3eb30873
amal jose <amal.jose@appsintegra.com>
2020-02-25 14:47:46 +0400

    Annotate working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import data from './data.json';
+import data from './data/data.json';
 import { makeTableString} from './helpers/makeTable';
 
 function App() {
--- my-app/src/helpers/addAnnotation.js
+++ my-app/src/helpers/addAnnotation.js
@@ -1,3 +1,16 @@
-export const addAnnotation = (key,value) => {
-    return `<span class="annotate-red">${value}<span>`
+import annotateData from '../data/annotateData.json';
+import { replaceAll } from './replaceAll';
+
+export const addAnnotation = (key, value) => {
+    let annotated = value
+    const currentAnnotations = annotateData[0].annotations.annotations[key]
+    if (currentAnnotations) {
+        currentAnnotations.inclusion.forEach(i => {
+            annotated = replaceAll(annotated, i, `<span class="annotate-green">${i}</span>`)
+        });
+        currentAnnotations.exclusion.forEach(i => {
+            annotated = replaceAll(annotated, i, `<span class="annotate-red">${i}</span>`)
+        });
+    }
+    return annotated
 }
